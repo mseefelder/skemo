530fd5c66c70a5d310d5b16106b2c554f6027651
Pedro Eusebio <pedrosantos95@gmail.com>
2016-03-03 20:41:53 -0300

    multi-mesh working

--- js/main.js
+++ js/main.js
@@ -2,29 +2,53 @@ var main = new function () {
 
     var self = this;
     var displaying;
+    var sketch1 = new sketch();
 
     this.init = function () {
+        
         var canvas2d = document.getElementById('2dcanvas');
         var canvas3d = document.getElementById('3dcanvas');
-
-        sketch.init2d(self, canvas2d);
+        
+        sketch1.init2d(self, canvas2d);
         world.init3d(self, canvas3d);
 
-        world.canvas3d.style.visibility = "hidden";
+        canvas3d.style.display = "none";
 
-        document.addEventListener("mousemove", sketch.mouseMoveHandle, false);
-        document.addEventListener("mousedown", sketch.mouseDownHandle, false);
+        document.addEventListener("mousemove", sketch1.mouseMoveHandle, false);
+        document.addEventListener("mousedown", sketch1.mouseDownHandle, false);
         //document.addEventListener("mouseup", sketch.mouseUpHandle, false);
-        document.addEventListener("mouseup", sketchToMesh, false);
-        document.addEventListener("mouseout", sketch.mouseOutHandle, false);
+        document.addEventListener("mouseup",function(){
+          sketchToMesh(event, sketch1); 
+        } , false);
+        canvas3d.addEventListener("mouseout", sketch1.mouseOutHandle, false);
     };
 
     //Function to be called on sketching mouseup to create mesh
-    function sketchToMesh (e) {
+    function sketchToMesh (e,sketch) {
         sketch.mouseUpHandle(e);
         world.buildObject(sketch.contour, sketch.steiner, sketch.arrayDistance, sketch.triangles);
         displaying = sketch.canvas2d.style.display;
         sketch.canvas2d.style.display = "none";
-        world.canvas3d.style.visibility = "visible";
+        world.canvas3d.style.display = "block";
     };
 }
+
+$(document).ready(function () {
+    main.init();
+    var canvas2d = document.getElementById('2dcanvas');
+    var canvas3d = document.getElementById('3dcanvas');
+
+    $('#edit').on('click', function() {
+        if(canvas2d.style.display == "none") {
+            canvas2d.style.display = 'block';
+            canvas3d.style.display = 'none';
+
+            var sketch1 =  new sketch();
+            sketch1.init2d(self, canvas2d);
+            main.init(sketch);
+        } else {
+            canvas3d.style.display = 'block';
+            canvas2d.style.display = "none"; 
+        }
+    });
+})
